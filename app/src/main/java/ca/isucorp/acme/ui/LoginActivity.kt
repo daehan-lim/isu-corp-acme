8dfc3ccfed2c9fc9e91138f60aface7fe87cea16
daehan-lim <penjan.eng@gmail.com>
2021-05-26 01:01:29 -0400

    Add data binding to LoginActivity.kt

--- app/src/main/java/ca/isucorp/acme/ui/LoginActivity.kt
+++ app/src/main/java/ca/isucorp/acme/ui/LoginActivity.kt
@@ -3,10 +3,14 @@ package ca.isucorp.acme.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ca.isucorp.acme.R
+import ca.isucorp.acme.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
