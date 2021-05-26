ae1102734a1d1a448ef83eec438e0a15fbf18d63
daehan-lim <penjan.eng@gmail.com>
2021-05-26 00:30:57 -0400

    Initial commit

---
+++ app/src/main/java/ca/isucorp/acme/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package ca.isucorp.acme.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.isucorp.acme.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
