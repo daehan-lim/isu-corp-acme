a571ab35d2c74940510c334d8ffe6e4f48407a1f
daehan-lim <penjan.eng@gmail.com>
2021-05-26 11:37:19 -0400

    Refactor toolbar in SignUpActivity and set up go back animations

--- app/src/main/java/ca/isucorp/acme/ui/login/SignUpActivity.kt
+++ app/src/main/java/ca/isucorp/acme/ui/login/SignUpActivity.kt
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity
 import ca.isucorp.acme.R
 import ca.isucorp.acme.databinding.ActivityLoginBinding
 import ca.isucorp.acme.databinding.ActivitySignUpBinding
+import ca.isucorp.acme.util.DEFAULT_GO_BACK_ANIMATION
+import ca.isucorp.acme.util.goBackWithAnimation
+import ca.isucorp.acme.util.setUpInActivity
 import ca.isucorp.acme.util.underlineText
 
 
@@ -19,8 +22,17 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar = binding.layoutAppBar.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val toolBarTitle = toolbar.findViewById<TextView>(R.id.toolbar_title)
+        toolBarTitle.text = getString(R.string.create_account)
+        toolbar.setUpInActivity(this, DEFAULT_GO_BACK_ANIMATION)
 
 
 
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        goBackWithAnimation(this, DEFAULT_GO_BACK_ANIMATION)
     }
 }
--- app/src/main/java/ca/isucorp/acme/util/Util.kt
+++ app/src/main/java/ca/isucorp/acme/util/Util.kt
@@ -3,6 +3,15 @@ package ca.isucorp.acme.util
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import ca.isucorp.acme.R
+
+
+/**
+ * Default animation id for pressing back button in Activity
+ */
+const val DEFAULT_GO_BACK_ANIMATION = R.anim.slide_out_right
 
 /**
  * Sets a span to underline the TextView's text
@@ -11,4 +20,21 @@ fun TextView.underlineText() {
     val content = SpannableString(text.toString())
     content.setSpan(UnderlineSpan(), 0, text.toString().length, 0)
     text = content
+}
+
+
+fun goBackWithAnimation(activity: AppCompatActivity, anim: Int?) {
+    activity.finish()
+    if(anim != null) {
+        activity.overridePendingTransition(0, anim)
+    }
+}
+
+fun Toolbar.setUpInActivity(activity: AppCompatActivity, goBackAnimation: Int?) {
+    activity.setSupportActionBar(this)
+    title = ""
+    setNavigationIcon(R.drawable.ic_arrow_circle_left_back)
+    setNavigationOnClickListener {
+        goBackWithAnimation(activity, goBackAnimation)
+    }
 }
