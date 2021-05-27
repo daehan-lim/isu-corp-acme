7915dc0c5e72e18ef3e9683884b0be1b1ebd9d98
daehan-lim <penjan.eng@gmail.com>
2021-05-27 14:05:18 -0400

    Fix that dropdown menu text size was being increased when not in tablet too

--- app/src/main/java/ca/isucorp/acme/ui/MainActivity.kt
+++ app/src/main/java/ca/isucorp/acme/ui/MainActivity.kt
@@ -16,6 +16,7 @@ import java.util.*
 const val PHONE_PERMISSION_CODE = 102
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isTabletSize: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         val toolbar = binding.layoutAppBar.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val isTabletSize = resources.getBoolean(R.bool.isTablet)
+        isTabletSize = resources.getBoolean(R.bool.isTablet)
         if(isTabletSize) {
             toolbar.layoutParams.height = resources.getDimension(R.dimen.action_bar_size).toInt()
         }
@@ -40,8 +41,10 @@ class MainActivity : AppCompatActivity() {
         val popup = PopupMenu(this, menuButton)
         popup.inflate(R.menu.dropdown_menu)
 
-        increaseMenuItemTextSize(popup, R.id.action_work_ticker)
-        increaseMenuItemTextSize(popup, R.id.action_get_directions)
+        if(isTabletSize) {
+            increaseMenuItemTextSize(popup, R.id.action_work_ticker)
+            increaseMenuItemTextSize(popup, R.id.action_get_directions)
+        }
 
         popup.setOnMenuItemClickListener { item: MenuItem? ->
             when (item!!.itemId) {
